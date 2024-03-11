39102060c1118faa7785371793a8cd8af8a645e6
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2024-03-10 23:56:55 -0300

    prolijo

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::thread::{self, sleep};
 use std::time::Duration;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-enum EstadoConexion {
-    Conectado,
-    Desconectado,
+enum ConnectionStatus {
+    Connected,
+    Disconnected,
 }
 
 fn run_process() -> Child {
@@ -20,40 +20,41 @@ fn run_process() -> Child {
 
 fn main() {
     let mut process_that_i_want = run_process();
-    println!("Process id {}", process_that_i_want.id());
+    println!("Process with id {} launched", process_that_i_want.id());
 
     let (tx, rx) = mpsc::channel();
-    let short_handle = thread::spawn(move || loop {
+    thread::spawn(move || loop {
         let output = Command::new("curl")
             .args(["http://localhost:3000"])
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .status()
-            .expect("No anduvo el comando");
+            .expect("Can't run listener command");
         if !output.success() {
-            tx.send(EstadoConexion::Desconectado).unwrap();
-            println!("ESTADO ERROR");
+            tx.send(ConnectionStatus::Disconnected).unwrap();
             sleep(Duration::from_secs(2));
         } else {
-            tx.send(EstadoConexion::Conectado).unwrap();
-            // println!("ESTADO Feliz");
+            tx.send(ConnectionStatus::Connected).unwrap();
             sleep(Duration::from_secs(2));
         }
     });
-    let mut estado_previo = EstadoConexion::Conectado;
+    let mut previous_state = ConnectionStatus::Connected;
     loop {
-        let estado_actual = rx.recv().unwrap();
-        if let EstadoConexion::Desconectado = estado_actual {
-            println!("[ERROR]: no hay conexion");
-            println!("Process id {}", process_that_i_want.id());
-            process_that_i_want.kill().expect("No lo puedo cerrar");
+        let current_state = rx.recv().unwrap();
+        if let ConnectionStatus::Disconnected = current_state {
+            if previous_state == ConnectionStatus::Connected {
+                println!("[DISCONNECTED]: Listener ping received no response from remote url");
+            }
+            process_that_i_want.kill().expect("Process cannot be killed");
            
         }
-        if estado_previo == EstadoConexion::Desconectado
-            && estado_actual == EstadoConexion::Conectado
+        if previous_state == ConnectionStatus::Disconnected
+            && current_state == ConnectionStatus::Connected
         {
-            process_that_i_want = run_process()
+            println!("Process restarted with PID = {}", process_that_i_want.id());
+
+            process_that_i_want = run_process();
         }
-        estado_previo = estado_actual;
+        previous_state = current_state;
     }    
 }
